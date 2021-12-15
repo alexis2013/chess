75bdc555022ffe4ceec1d62d9feb176e82721e22
alexis.albanese <alexisalbanese2017@gmail.com>
2021-12-15 18:47:22 -0300

    add api folder

--- main.py
+++ main.py
@@ -9,5 +9,12 @@ def indice():
     return render_template('index.html')
 
 
+@app.route('/form-data', methods=['POST'])
+def formulario():
+    if request.method == 'POST':
+        print(request)
+
+    return url_for('/')
+
 if __name__ == '__main__':
     app.run(debug=True)
